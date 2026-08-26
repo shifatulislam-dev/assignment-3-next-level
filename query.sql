e7ae2ed1dec7db8c258e0332c1e72c6c5d54ca02
shifatulislam-dev <shifatulislam794@gmail.com>
2026-08-26 14:53:18 +0600

    complete query 7

--- query.sql
+++ query.sql
@@ -156,3 +156,13 @@ WHERE total_cost > (
     SELECT AVG(total_cost)
     FROM Bookings
 );
+
+-- QUERY 7
+SELECT
+    match_id,
+    fixture,
+    base_ticket_price
+FROM Matches
+ORDER BY base_ticket_price DESC
+OFFSET 1
+LIMIT 2;
